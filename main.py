49cee31c2cce226f99f1449dbf36e49d6f6e6416
FABIO L SANTOS <78166985+fabiolucasz@users.noreply.github.com>
2025-06-02 17:59:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,32 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+class ProductsSchema(BaseModel):
+    name: str
+    description: str
+    qnt_products: int
+    price: float
+    image: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
 def home_page():
     return ("Acesse /docs")
 
 @app.post("/product")
-def new_product():
-    return ("Cadastrar")
+def new_product(product: ProductsSchema, db: Session = Depends(get_db)):
+    product = Products(name=product.name,
+                        description=product.description,
+                        qnt_products=product.qnt_product,
+                        price=product.price,
+                        image=product.image)
+    db.add(product)
+    db.commit()
+    db.refresh(product)
+    return product
